19cf693bdf4c93cea948017ec0f3cd413f0a1f43
Servidor TS Ts <40902224-tsvoisp@users.noreply.replit.com>
2025-08-19 21:26:35 +0000

    commit att 19-08-2025

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,27 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "aet-server",
-      script: "server/dist/index.js", // caminho do seu build do backend
+      name: "aet-license-system",
+      script: "server/dist/index.js",
       interpreter: "node",
       instances: 1,
       autorestart: true,
       watch: false,
       env: {
         NODE_ENV: "production",
+        DATABASE_URL: "postgresql://aetuser:nvs123@localhost:5432/aetlicensesystem",
         PORT: 5050
-      }
-    },
-    {
-      name: "aet-client",
-      script: "node_modules/vite/bin/vite.js",
-      args: "preview --host 0.0.0.0 --port 5000",
-      interpreter: "node",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      env: {
-        NODE_ENV: "production"
       }
     }
   ]
